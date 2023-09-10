a1871a61a7cb2770623890dd0779adc8a91c1a0c
jsmtcode <smartaustin37@gmail.com>
2023-09-10 05:24:06 -0700

    second commit

--- app/views.py
+++ app/views.py
@@ -12,22 +12,22 @@ def get_info(request):
     slack_name = request.GET.get('slack_name')
     track = request.GET.get('track')
     github_url = f"https://github.com/jsmtcode/HNGx_projects"
-    github_source_url = "https://github.com/your/repo/source_code.py"
+    github_source_url = "https://github.com/jsmtcode/HNGx_projects/blob/master/app/views.pyclear"
 
     # Calculate the current day of the week
     current_day = datetime.now().strftime('%A')
 
     # Calculate the current UTC time with validation of +/-2 hours
     current_utc_time = datetime.utcnow()
-    current_utc_time -= timedelta(hours=2) 
+    current_utc_time += timedelta(hours=1) 
 
     response_data = {
-        'slack_name': "iheanyi_okeh",
-        'current_day_of_week': current_day,
-        'current_utc_time': current_utc_time.isoformat(),
-        'track': "Backend",
-        'github_url_of_file_being_run': github_url,
-        'github_url_of_file_source_code': github_source_url,
+        'slack_name': slack_name,
+        'current_day': current_day,
+        'utc_time': current_utc_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        'track': track,
+        'github_file_url': github_source_url,
+        'github_repo_url': github_url,
         'status_code': 200
     }
 
